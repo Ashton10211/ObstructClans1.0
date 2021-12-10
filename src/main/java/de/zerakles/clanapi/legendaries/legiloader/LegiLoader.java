868c2368e16a33a916d0985ff24fc5ddf0aa2bc8
Zerakles <jonas@grewe.eu>
2021-12-10 19:35:53 +0100

    - changes LegiLoader.java

--- src/main/java/de/zerakles/clanapi/legendaries/legiloader/LegiLoader.java
+++ src/main/java/de/zerakles/clanapi/legendaries/legiloader/LegiLoader.java
@@ -1,4 +1,38 @@
 package de.zerakles.clanapi.legendaries.legiloader;
 
+import de.zerakles.clanapi.legendaries.Legend;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.UUID;
+
 public class LegiLoader {
+
+    private boolean doubleLegi;
+
+    private UUID loaderUuid;
+
+    public LegiLoader(UUID loaderUuid, boolean doubleLegi){
+        this.doubleLegi = doubleLegi;
+        this.loaderUuid = loaderUuid;
+        return;
+    }
+
+    public boolean isDoubleLegi() {
+        return doubleLegi;
+    }
+
+    public UUID getLoaderUuid() {
+        return loaderUuid;
+    }
+
+    public void setDoubleLegi(boolean doubleLegi) {
+        this.doubleLegi = doubleLegi;
+    }
+
+    public void setLoaderUuid(UUID loaderUuid) {
+        this.loaderUuid = loaderUuid;
+    }
+
+    public HashMap<Legend, Player> AlligatorsTooths = new HashMap<>();
 }
