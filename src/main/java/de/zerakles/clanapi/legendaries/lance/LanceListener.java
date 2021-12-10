5e7c205ec43b707143602498ba09cfe86e5ed0c8
Zerakles <jonas@grewe.eu>
2021-12-10 23:11:23 +0100

    - updated Lance.java - updated LanceListener.java

--- src/main/java/de/zerakles/clanapi/legendaries/lance/LanceListener.java
+++ src/main/java/de/zerakles/clanapi/legendaries/lance/LanceListener.java
@@ -4,42 +4,82 @@ import de.zerakles.clanapi.ClanAPI;
 import de.zerakles.clanapi.legendaries.Legend;
 import de.zerakles.main.Clan;
 import de.zerakles.utils.Data;
+import de.zerakles.utils.Display;
 import de.zerakles.utils.Utils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class LanceListener implements Listener {
-    private Clan getClan(){
+    private Clan getClan() {
         return Clan.getClan();
     }
-    private ClanAPI getClanAPI(){
+
+    private ClanAPI getClanAPI() {
         return getClan().getClanAPI();
     }
-    private Data getData(){
+
+    private Data getData() {
         return getClan().data;
     }
 
     public HashMap<Player, Legend> Lances = new HashMap<>();
 
-    public LanceListener(){
+    public LanceListener() {
         loop();
     }
 
-    public void loop(){}
+    public void loop() {
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(getClan(), new Runnable() {
+            @Override
+            public void run() {
+
+                for (Player p : Cooldown.keySet()) {
+                    if (p == null || !p.isOnline()) {
+                        Cooldown.remove(p);
+                        continue;
+                    }
+                    if ((System.currentTimeMillis() - Cooldown.get(p)) / 1000L > 12L) {
+                        Cooldown.remove(p);
+                        p.sendMessage(getData().prefix + ChatColor.GRAY +
+                                "You can use " + ChatColor.GREEN + "Lance Run");
+                        if (isCorrectItem(p.getItemInHand(), p))
+                            Display.display(ChatColor.GREEN + " Lance Run" + " Recharged", p);
+                        p.playSound(p.getLocation(), Sound.valueOf("NOTE_PLING"), 5.0F, 1.0F);
+                        continue;
+                    }
+                    if (isCorrectItem(p.getItemInHand(), p)) {
+                        Double x = 16.0D - Math.pow(10.0D, -1.0D) * ((System.currentTimeMillis() - (Long) Cooldown.get(p)) / 100L);
+                        double divide = (System.currentTimeMillis() - (Long) Cooldown.get(p) - 4.5D) / 16000.0D;
+                        String[] zz = x.toString().replace('.', '-').split("-");
+                        String concat = zz[0] + "." + zz[1].charAt(0);
+                        Display.displayProgress("§eLance Run", divide,
+                                ChatColor.WHITE + " " + concat + " §eSeconds", false, p);
+                    }
+                }
+            }
+        }, 0, 1);
+    }
 
     private boolean isCorrectItem(ItemStack item, Player p) {
-        if(Lances.containsKey(p)){
-            if(!item.hasItemMeta()){
+        if (Lances.containsKey(p)) {
+            if (!item.hasItemMeta()) {
                 return false;
             }
-            if(item.getItemMeta().equals(Lances.get(p).getItemStack().getItemMeta())){
+            if (item.getItemMeta().equals(Lances.get(p).getItemStack().getItemMeta())) {
                 return true;
             }
             return false;
@@ -47,22 +87,56 @@ public class LanceListener implements Listener {
         return false;
     }
 
-    ArrayList<Player>firstHit = new ArrayList<>();
+    ArrayList<Player> firstHit = new ArrayList<>();
     HashMap<Player, Long> stunnedPlayer = new HashMap<>();
+    HashMap<Player, Long> Cooldown = new HashMap<>();
+
+    @EventHandler
+    public void onClick(PlayerInteractEvent playerInteractEvent) {
+        if (isCorrectItem(playerInteractEvent.getPlayer().getItemInHand(), playerInteractEvent.getPlayer())) {
+            Player player = playerInteractEvent.getPlayer();
+            ItemStack itemStack = player.getItemInHand();
+            if (playerInteractEvent.getAction() == Action.RIGHT_CLICK_AIR
+                    || playerInteractEvent.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                if (Cooldown.containsKey(player)) {
+                    if (System.currentTimeMillis() - Cooldown.get(player) / 1000L == 12L
+                            || System.currentTimeMillis() - Cooldown.get(player) / 1000L > 12L) {
+                        Cooldown.remove(player);
+                        player.sendMessage(getData().prefix + "§7Your §aRun §7is ready!");
+                        return;
+                    }
+                    Double x = 16.0D - Math.pow(10.0D, -1.0D) * ((System.currentTimeMillis() - (Long) Cooldown.get(player.getName())) / 100L);
+                    String[] zz = x.toString().replace('.', '-').split("-");
+                    String concat = zz[0] + "." + zz[1].charAt(0);
+                    player.sendMessage(getData().prefix + ChatColor.GRAY +
+                            "Your cannot use " + ChatColor.GREEN + "Lance Run" + ChatColor.GRAY +
+                            " for " + ChatColor.GREEN +
+                            concat + " Seconds");
+                } else {
+                    Cooldown.put(player, System.currentTimeMillis());
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 20 * 6, 6));
+                    player.playSound(player.getLocation(), Sound.HORSE_GALLOP, 1F, 1F);
+                    player.sendMessage(getData().prefix + ChatColor.GRAY + "You used " + ChatColor.GREEN +
+                            "Lance Run.");
+                    return;
+                }
+            }
+        }
+    }
 
     @EventHandler
     public void onDmg(EntityDamageByEntityEvent e) {
         if (e.getDamager() instanceof Player) {
-            Player p = (Player)e.getDamager();
+            Player p = (Player) e.getDamager();
             ItemStack item = Utils.getItemInHand(p);
-            if (isCorrectItem(item,p)) {
+            if (isCorrectItem(item, p)) {
                 e.setDamage(Lances.get(p).getDamage());
-                if(!(e.getEntity() instanceof Player)){
+                if (!(e.getEntity() instanceof Player)) {
                     return;
                 }
-                if(firstHit.contains(p)){
+                if (firstHit.contains(p)) {
                     stunnedPlayer.put((Player) e.getEntity(), System.currentTimeMillis());
-                    e.getEntity().setFireTicks(20*3);
+                    e.getEntity().setFireTicks(20 * 3);
                     p.sendMessage(getData().prefix + "§7Player stunned!");
                     firstHit.remove(p);
                 } else {
@@ -73,10 +147,10 @@ public class LanceListener implements Listener {
     }
 
     @EventHandler
-    public void onMove(PlayerMoveEvent playerMoveEvent){
-        if(stunnedPlayer.containsKey(playerMoveEvent.getPlayer())){
-            if(System.currentTimeMillis()-stunnedPlayer.get(playerMoveEvent.getPlayer()) == 1L
-                ||  System.currentTimeMillis()-stunnedPlayer.get(playerMoveEvent.getPlayer()) / 1000L > 0.8){
+    public void onMove(PlayerMoveEvent playerMoveEvent) {
+        if (stunnedPlayer.containsKey(playerMoveEvent.getPlayer())) {
+            if (System.currentTimeMillis() - stunnedPlayer.get(playerMoveEvent.getPlayer()) == 1L
+                    || System.currentTimeMillis() - stunnedPlayer.get(playerMoveEvent.getPlayer()) / 1000L > 0.8) {
                 stunnedPlayer.remove(playerMoveEvent.getPlayer());
                 return;
             }
