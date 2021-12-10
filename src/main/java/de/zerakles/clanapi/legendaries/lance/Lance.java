76abfdbd974a54d58f259d2eefea19fddbc6fb7a
Zerakles <jonas@grewe.eu>
2021-12-10 20:26:57 +0100

    - added Lance.java - added LanceListener.java

---
+++ src/main/java/de/zerakles/clanapi/legendaries/lance/Lance.java
@@ -0,0 +1,10 @@
+package de.zerakles.clanapi.legendaries.lance;
+
+import de.zerakles.clanapi.legendaries.Legend;
+
+public class Lance {
+    public Legend legend;
+    public Lance(){
+
+    }
+}
---
+++ src/main/java/de/zerakles/clanapi/legendaries/lance/LanceListener.java
@@ -0,0 +1,4 @@
+package de.zerakles.clanapi.legendaries.lance;
+
+public class LanceListener {
+}
